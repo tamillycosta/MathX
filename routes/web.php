5820a18967093757b30bbd350a1e7e7d5ce4c617
Tamilly costa <Tamillycosta899@gmail.com>
2025-02-09 17:00:20 -0300

    aplica middleware as rotas de printar e exportar os exercicios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\hasExercises;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::controller(MainController::class)->group(function(){
+
+    Route::get('/', 'home')->name('home');
+    Route::post('/generateExercises', 'genareteExecises')->name('genarete');
+    Route::get('/printExercies', 'printExercies')->name('print')->middleware(hasExercises::class);
+    Route::get('/exportExercies','exportExercies' )->name('export')->middleware(hasExercises::class);
+}); 
