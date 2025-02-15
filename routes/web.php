905fb13dd29c33bba0ab462567aa0a6132ba87c2
Tamilly costa <Tamillycosta899@gmail.com>
2025-02-15 19:36:37 -0300

    feat: adiciona rotas para loop do jogo

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
 use App\Http\Controllers\ExerciseController;
+use App\Http\Controllers\GameController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\hasExercises;
 
 
-
+// Rotas da pagina principal 
 Route::controller(MainController::class)->group(function(){
-
-    
     Route::get('/', 'home')->name('home');
     Route::get('/generates', 'exercisesHome')->name('exercisesHome');
-
+    Route::get('/game','gameHome')->name('game');
    
 }); 
+
 
 // Rotas do gerador de exercicios 
     Route::controller(ExerciseController::class)->group(function(){
@@ -25,4 +25,18 @@ Route::controller(MainController::class)->group(function(){
         Route::get('/export','exportExercies' )->name('export')->middleware(hasExercises::class);
     });
     
+});
+
+
+//Rotas do game 
+Route::controller(GameController::class)->group(function(){
+    Route::prefix('game')->group(function(){
+        
+        Route::post('/setLevel', 'gameLevel')->name('setLevel');
+        Route::get('/question',  'gameLoop')->name('game.question');
+        Route::post('/next',  'nextQuestion')->name('game.next');
+        Route::get('/answer/{answer}', 'getAnswer')->name('game.answer');
+        Route::get('/next', 'nextQuestion')->name('game.next');
+        Route::get('/end', 'gameEnd')->name('game.end');
+    });
 });
