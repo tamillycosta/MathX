7e6f9659bb395f7463bab15b0b2e71e7f0e69109
Tamilly costa <Tamillycosta899@gmail.com>
2025-02-15 19:35:34 -0300

    feat: implementa loop do jogo

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Services\Game\Factory\GameFactory;
 use App\Http\Requests\LevelPostRequest;
+use App\Services\Game\GameService;
 use Illuminate\View\View;
 
 class GameController extends Controller
@@ -12,31 +13,78 @@ class GameController extends Controller
             return view(view: 'game.home');
     }
 
-    private function setLevel(LevelPostRequest $request){
+    private function setLevel(LevelPostRequest $request): object{
         $level = $request->level;
         $class = GameFactory::selectLevel($level);
         return $class;
     }
 
+
     public function gameLevel(LevelPostRequest $request){
-
         $class = $this->setLevel($request);
-
         $questions = $class->setGameLevel(5);
         $answer =  $class->getAnswer($questions);
-        $whrongAnswer = $class->getWrongAnswer($questions);
-
-       $data = [
-        'questions' => $questions,
-        'answer' => $answer,
-        'whrongAnswer' => $whrongAnswer
-       ];
-    
-       
-       print_r($whrongAnswer);
-        //return view('game.game', ['data' => $data]);
+        $wrongAnswer = $class->getWrongAnswer($questions);
+        GameService::gameConfig($questions, $answer, $wrongAnswer);
+     
+        return redirect()->route('game.question');
     }
 
+    public function gameLoop(){
+        $questions = session('questions', []);
+        $index = session('questionIndex', 0);
+        
+        if ($index >= count($questions)) {
+            return redirect()->route('game.end'); // Redireciona ao final
+        }
+
+       return $this->showQuestion();
+    }
+
+    public function showQuestion(){
+
+        $questions = session('questions', []);
+        $answers = session('answers', []);
+        $wrongAnswers = session('wrongAnswers', []);
+
+        $index = session('questionIndex', 0);
+        $totalQuestions = session('totalQuestions', 0);
+
+
+        $question = $questions[$index];
+        $correctAnswer = $answers[$index];
+        $incorrectAnswers = $wrongAnswers[$index];
+
+        $options = $incorrectAnswers;
+        $options[] = $correctAnswer;
+        shuffle($options);
+
+        return view('game.game', compact('question', 'options', 'index', 'totalQuestions'));
+        }
+
+
+        public function nextQuestion() {
+            $index = session('questionIndex', 0) + 1;
+            session(['questionIndex' => $index]);
+        
+            return redirect()->route('game.question');
+        }
+
+
+        function getAnswer($answer){
+            $questions = session('questions', []);
+            $index = session('questionIndex', 0);
+            $totalQuestions = session('totalQuestions', 0);
+            $question = $questions[$index];
+
+            $data = GameService::feedbackAnswer($answer);
+            
+            return view('game.feedback', compact('data', 'question', 'index', 'totalQuestions' ));
+            
+        }
 
     
+        function gameEnd(){
+
+        }
 }
